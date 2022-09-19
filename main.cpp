6e46f38e121daa36c3e97eb5adaa468fb7316f22
CMDR-DiTRay <philip.zakharov@gmail.com>
2022-09-19 21:12:44 +0300

    Initial code commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ImageMagick-6/Magick++.h>
+
+using namespace std;
+using namespace Magick;
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
